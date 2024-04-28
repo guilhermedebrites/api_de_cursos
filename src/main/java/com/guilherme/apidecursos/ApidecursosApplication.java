587eb76cc10c42391e0adb865e873ecd6d90423c
Guilherme DeBrites <100132323+guilhermedebrites@users.noreply.github.com>
2024-04-28 16:39:05 -0300

    Start Project

---
+++ src/main/java/com/guilherme/apidecursos/ApidecursosApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.apidecursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidecursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidecursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/apidecursos/ApidecursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.apidecursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidecursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
